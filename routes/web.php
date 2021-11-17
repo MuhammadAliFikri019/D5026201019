965946fad25bdec980c3769e8629aba2dce71152
MuhammadAliFikri019 <muhammadalifikri948@gmail.com>
2021-11-17 17:19:26 +0700

    Tutorial CRUD
    Mahasiswa melakukan tutorial Create dan Update dan sudah melakukan tugas Update dan Delete

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,15 @@ Route::get('ets2021',"ViewController@showETS") ;
 
 Route::get('tugaske6php',"ViewController@showTugasphp") ;
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
 // Route::get('greetings',"ViewController@showGreetings") ;
 // Route::post('sayhi',"ViewController@sayHi") ;
+
 
